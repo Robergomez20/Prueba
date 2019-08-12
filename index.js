94dee92507892877711a51d11a3ce29cf6e7dde7
Robergomez20 <54005329+Robergomez20@users.noreply.github.com>
2019-08-11 20:50:02 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-
-/*const {consultarclima} = require('consultarclima');
-
-const clima = new consultarclima('Bogota');
-
-
-async function fetconsultaclima(){
-  const data = await consultarclima.getconsultarclima();
-  console.log(data);
-}
-document.addEventListener('DOMContentLoaded', consultarclima);
-*/
 $("#consultar").click(function(){
 
   var url = 'http://api.openweathermap.org/data/2.5/weather?q='+$("#ciudad").val()+'&appid=5b2cba3336d6c337ec54d7f9499a4c45&units=metric';
@@ -19,7 +7,7 @@ $("#consultar").click(function(){
   dataType: 'json',
   success: function( response ) {
     //console.log(response);
-      $("#tempClima").text(response.main.temp);
+      $("#tempClima").text(response.main.temp + '°C');
       $("#pronosticoClima").text(response.weather[0].description);
       $("#imgPronostico").prop("src",'http://openweathermap.org/img/wn/'+response.weather[0].icon+'@2x.png')
       ConsultarClima5Dias();
@@ -33,7 +21,7 @@ $("#consultar").click(function(){
 
 function ConsultarClima5Dias(){
 
-  var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+$("#ciudad").val()+'&mode=json&units=metric&cnt=5&appid=5b2cba3336d6c337ec54d7f9499a4c45';
+  var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+$("#ciudad").val()+'&appid=5b2cba3336d6c337ec54d7f9499a4c45';
 
   $.ajax({
   url: url,
